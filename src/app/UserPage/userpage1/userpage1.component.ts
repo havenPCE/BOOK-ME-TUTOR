c874f4ae6fe36319e47298cb16a3d501c46dc20e
Sangeeta <ssangeeta2511@gmail.com>
2020-02-22 22:27:09 +0530

    commit

---
+++ src/app/UserPage/userpage1/userpage1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userpage1',
+  templateUrl: './userpage1.component.html',
+  styleUrls: ['./userpage1.component.css']
+})
+export class Userpage1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
